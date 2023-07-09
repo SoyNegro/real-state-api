f649dae35dcd61a137757c9c3c28885e99ddbf54
SoyNegro <krisoul.valdes@gmail.com>
2023-07-09 16:43:43 -0400

    CostType enum added

---
+++ src/main/java/dev/coffecult/realstate/model/enums/CostType.java
@@ -0,0 +1,5 @@
+package dev.coffecult.realstate.model.enums;
+
+public enum CostType {
+       SecurityDeposit, AdministrationFee, Water, Gas, Electricity, Internet, CleaningService, FinalCleaning, OvernightGuests, EarlyMoveIn, LateMoveIn,LateMoveOut, OptionalCosts
+}
