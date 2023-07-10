ca2ee74c41b36b72a0c1023c3852995a1d997d6d
SoyNegro <krisoul.valdes@gmail.com>
2023-07-10 16:32:47 -0400

    Added validation for Address

--- src/main/java/dev/coffecult/realstate/model/components/Address.java
+++ src/main/java/dev/coffecult/realstate/model/components/Address.java
@@ -1,12 +1,21 @@
 package dev.coffecult.realstate.model.components;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record Address(
+        @NotEmpty
         String number,
+        @NotEmpty
         String street,
+        @NotEmpty
         String neighborhood,
+        @NotEmpty
         String city,
+        @NotEmpty
         String state,
+        @NotEmpty
         String countryCode,
+        @NotEmpty
         String postalCode
-        ) {
+) {
 }
