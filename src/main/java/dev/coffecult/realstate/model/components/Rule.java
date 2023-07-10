7059c468102a6cfdc749d1dc0073e3d737d6aae2
SoyNegro <krisoul.valdes@gmail.com>
2023-07-10 16:49:22 -0400

    Added validation for Rule

--- src/main/java/dev/coffecult/realstate/model/components/Rule.java
+++ src/main/java/dev/coffecult/realstate/model/components/Rule.java
@@ -1,11 +1,15 @@
 package dev.coffecult.realstate.model.components;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import dev.coffecult.realstate.model.enums.AllowedInProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
 public class Rule {
     private AllowedInProperty petsAllowed;
     private boolean cleaningCommonRooms;
