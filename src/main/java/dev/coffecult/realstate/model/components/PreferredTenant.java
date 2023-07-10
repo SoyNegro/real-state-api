484ebf6715faf81f7a3a64a8de48edad73ac088a
SoyNegro <krisoul.valdes@gmail.com>
2023-07-10 16:46:04 -0400

    Added validation for PreferredTenant

--- src/main/java/dev/coffecult/realstate/model/components/PreferredTenant.java
+++ src/main/java/dev/coffecult/realstate/model/components/PreferredTenant.java
@@ -1,18 +1,30 @@
 package dev.coffecult.realstate.model.components;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import dev.coffecult.realstate.model.enums.Gender;
+import dev.coffecult.realstate.model.enums.TenantStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PreferredTenant {
+    @NotNull
+    @Min(0)
     private short minAge;
+    @Min(0)
     private short maxAge;
+    @NotNull
     private Gender gender;
 
-    // ie: Students, working professionals
-    private String tenantType;
+    @NotNull
+    private TenantStatus tenantStatus;
 
+    @NotNull
     private boolean suitableForCouples;
 }
