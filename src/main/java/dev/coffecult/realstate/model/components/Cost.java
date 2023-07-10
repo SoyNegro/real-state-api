bb0d4831cf91551787d01f25d90cef655a4c132a
SoyNegro <krisoul.valdes@gmail.com>
2023-07-10 16:44:36 -0400

    Added validation for Cost

--- src/main/java/dev/coffecult/realstate/model/components/Cost.java
+++ src/main/java/dev/coffecult/realstate/model/components/Cost.java
@@ -2,6 +2,8 @@ package dev.coffecult.realstate.model.components;
 
 import dev.coffecult.realstate.model.enums.CostType;
 import dev.coffecult.realstate.model.enums.PayableAt;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,10 +12,17 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class Cost{
+    @NotNull
     private CostType type;
+    @NotNull
+    @Min(value = 0,message = "Price cannot be lower than 0")
     private int price;
+    @NotNull
     private PayableAt payableAt;
+    @NotNull
     private boolean includedInRent;
+    @NotNull
     private boolean refundable;
+    @NotNull
     private boolean required;
 }
