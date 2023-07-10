c472bb71bb6f420a0e8da99c04d066657e97a3c9
SoyNegro <krisoul.valdes@gmail.com>
2023-07-10 16:46:50 -0400

    Added validation for UnavailableDate

--- src/main/java/dev/coffecult/realstate/model/components/UnavailableDate.java
+++ src/main/java/dev/coffecult/realstate/model/components/UnavailableDate.java
@@ -1,6 +1,9 @@
 package dev.coffecult.realstate.model.components;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+
 import java.time.LocalDateTime;
 
-public record UnavailableDate(LocalDateTime dateFrom, LocalDateTime dateTo) {
+public record UnavailableDate(@FutureOrPresent LocalDateTime dateFrom, @Future LocalDateTime dateTo) {
 }
