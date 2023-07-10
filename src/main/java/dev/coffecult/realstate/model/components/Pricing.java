b84bd365be4f4f75aa51b839707d3132db6c99b1
SoyNegro <krisoul.valdes@gmail.com>
2023-07-10 16:46:37 -0400

    Added validation for Pricing

--- src/main/java/dev/coffecult/realstate/model/components/Pricing.java
+++ src/main/java/dev/coffecult/realstate/model/components/Pricing.java
@@ -1,7 +1,10 @@
 package dev.coffecult.realstate.model.components;
 
+
 import dev.coffecult.realstate.model.enums.PricingType;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -9,12 +12,16 @@ import java.util.List;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Pricing {
     // Price in the lowest denomination in given currency
+    @NotNull
     private int price;
     // Prices may vary since some place have seasonal prices.
     private List<Integer> monthlyPrices =  new ArrayList<>(12);
+    @NotNull
     private PricingType pricingType;
     // Currency code of the currency used in ISO 4217 format.
+    @NotNull
     private String currencyCode;
 }
