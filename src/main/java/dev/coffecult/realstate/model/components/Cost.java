87b4529467c83de8845ce13f2de972f5d63fd0b3
SoyNegro <krisoul.valdes@gmail.com>
2023-07-11 15:07:41 -0400

    Changed Cost to Record

--- src/main/java/dev/coffecult/realstate/model/components/Cost.java
+++ src/main/java/dev/coffecult/realstate/model/components/Cost.java
@@ -4,25 +4,20 @@ import dev.coffecult.realstate.model.enums.CostType;
 import dev.coffecult.realstate.model.enums.PayableAt;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class Cost{
-    @NotNull
-    private CostType type;
-    @NotNull
-    @Min(value = 0,message = "Price cannot be lower than 0")
-    private int price;
-    @NotNull
-    private PayableAt payableAt;
-    @NotNull
-    private boolean includedInRent;
-    @NotNull
-    private boolean refundable;
-    @NotNull
-    private boolean required;
+public record Cost(
+        @NotNull
+        CostType type,
+        @NotNull
+        @Min(value = 0, message = "Price cannot be lower than 0")
+        int price,
+        @NotNull
+        PayableAt payableAt,
+        @NotNull
+        boolean includedInRent,
+        @NotNull
+        boolean refundable,
+        @NotNull
+        boolean required
+) {
 }
