628162602cdbc90267b6b119e9308ddb6425d2fa
SoyNegro <krisoul.valdes@gmail.com>
2023-07-11 15:08:43 -0400

    Changed Pricing to Record

--- src/main/java/dev/coffecult/realstate/model/components/Pricing.java
+++ src/main/java/dev/coffecult/realstate/model/components/Pricing.java
@@ -3,25 +3,26 @@ package dev.coffecult.realstate.model.components;
 
 import dev.coffecult.realstate.model.enums.PricingType;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
-public class Pricing {
-    // Price in the lowest denomination in given currency
-    @NotNull
-    private int price;
-    // Prices may vary since some place have seasonal prices.
-    private List<Integer> monthlyPrices =  new ArrayList<>(12);
-    @NotNull
-    private PricingType pricingType;
-    // Currency code of the currency used in ISO 4217 format.
-    @NotNull
-    private String currencyCode;
+public record Pricing(
+        @NotNull
+        int price,
+        List<Integer> monthlyPrices,
+        @NotNull
+        PricingType pricingType,
+        @NotNull
+        String currencyCode
+) {
+        /**
+         * When price is of type 'flat' there is not need for the monthly prices
+         * */
+    Pricing(@NotNull
+            int price, @NotNull
+            String currencyCode) {
+        this(price, null, PricingType.Flat, currencyCode);
+    }
+
 }
+
